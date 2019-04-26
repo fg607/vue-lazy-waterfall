9f32b7359cb4d73a902a792f46a43029130a1977
fg607 <fg607@sina.com>
2019-04-26 14:49:41 +0800

    fix loading finished bug
    fix the bug that loading finished but the loading icon is not disappear

--- src/vue-lazy-waterfall/index.vue
+++ src/vue-lazy-waterfall/index.vue
@@ -258,6 +258,7 @@
 
               self.loadings--
               if (self.loadings === 0) {
+                self.finished = true
                 self.$emit('finished') //all loadings are finished
               }
               self.$emit('done', currentRenderItems.items)
